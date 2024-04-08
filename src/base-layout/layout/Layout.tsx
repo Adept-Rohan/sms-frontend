68f925c4f2faa18a52f0c1c042a3cd826ef2dd88
Adept-Rohan <rohan1d20@gmail.com>
2024-04-08 16:08:35 +0545

    fix(api): fixed base layout issue

--- src/base-layout/layout/Layout.tsx
+++ src/base-layout/layout/Layout.tsx
@@ -5,7 +5,7 @@ export const Layout = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="h-screen overflow-y-auto bg-[#E7F5FE] w-[calc(100%-240px)]">
+      <div className="h-screen overflow-y-auto bg-[#E7F5FE] w-[calc(100%-29  290px)]">
         <Outlet />
       </div>
     </div>
