bf672417b4f2f52e4ef4c793ab0e5e2bd5dd7b10
Adept-Rohan <rohan1d20@gmail.com>
2024-04-10 14:57:00 +0545

    updated routes for new pages

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,6 +3,9 @@ import Login from "../pages/Login";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 import { Layout } from "../base-layout/layout/Layout";
 import Home from "../pages/Home";
+import Department from "../pages/Department";
+import User from "../pages/User";
+import Notice from "../pages/Notice";
 
 const router = createBrowserRouter([
   {
@@ -18,8 +21,20 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: "/home",
+        path: "/",
         element: <Home />,
+      },
+      {
+        path: "/department",
+        element: <Department />,
+      },
+      {
+        path: "/user",
+        element: <User />,
+      },
+      {
+        path: "/notice",
+        element: <Notice />,
       },
     ],
   },
