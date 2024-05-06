3558958d679416af5f7f04f5dae897a2a2fb4d46
Adept-Rohan <rohan1d20@gmail.com>
2024-05-07 01:04:21 +0545

    created a new props for disabling search in floor

--- src/components/TableHeading.tsx
+++ src/components/TableHeading.tsx
@@ -1,29 +1,38 @@
 import { AddIcon } from "../icons/AddIcon";
 import { SearchIcon } from "../icons/SearchIcon";
 
-const TableHeading = ({ title }: { title: string }) => {
+const TableHeading = ({
+  title,
+  isSearchDisable,
+}: {
+  title: string;
+  isSearchDisable?: boolean;
+}) => {
   return (
     <div className="w-full h-[76px] px-[20px] flex items-center justify-between">
-      <h1 className="text-[#074E78] font-semibold font-system not-italic text-[16px]">
+      <h1 className="text-blue-900 font-quicksand font-semibold text-base ">
         {title}
       </h1>
-      <div className="flex relative">
-        <input
-          className="text-[#C5C5C5] w-[287px] h-[32px] rounded-[360px] border-solid border-[2px] focus:outline-blue-300 border-[#C5C5C5] outline-slate-200 px-[12px] bg-transparent py-[10px]"
-          type="text"
-          placeholder="Search"
-        />
-        <SearchIcon
-          width={24}
-          height={24}
-          color="#C5C5C5"
-          className="absolute right-3 top-1"
-        />
-      </div>
+      {isSearchDisable === false ? (
+        <div className="flex relative">
+          <input
+            className="text-[#C5C5C5] w-[287px] h-[32px] rounded-[360px] border-solid border-[2px] focus:outline-blue-300 border-[#C5C5C5] outline-slate-200 px-[12px] bg-transparent py-[10px]"
+            type="text"
+            placeholder="Search"
+          />
+
+          <SearchIcon
+            width={24}
+            height={24}
+            color="#C5C5C5"
+            className="absolute right-3 top-1"
+          />
+        </div>
+      ) : null}
       <div className="flex gap-6">
         <button
           className="inline-flex rounded-[360px] items-center justify-center bg-[#25A6F2]  gap-[8px] py-[4px] pl-[12px] pr-[20px]"
-          onClick={() => open()}
+          onClick={() => console.log("clicked")}
         >
           <div>
             <AddIcon width={24} height={24} color="#fff" />
